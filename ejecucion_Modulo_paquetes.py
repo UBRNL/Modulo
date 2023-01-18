4e4cd9e996df95e483c57d0944b8851e6b166d2f
Ubernel Hernandez Rios <conteraderiva22@gmail.com>
2023-01-17 20:35:34 -0500

    INGRESO MODULO

---
+++ ejecucion_Modulo_paquetes.py
@@ -0,0 +1,6 @@
+from matematica import aritmetica
+
+print(aritmetica.sumar(32, 22))
+print(aritmetica.restar(32, 22))
+print(aritmetica.mult(3, 22))
+print(aritmetica.div(32, 8))
---
+++ matematica/geometria.py
@@ -0,0 +1,5 @@
+def areaRectangulo(base:int, altura:int)-> int:
+    return base * altura
+
+def areaCirculo(pi:float , radio: int)-> float: 
+     return pi * (radio * radio)
